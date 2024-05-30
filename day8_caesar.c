f58289d3bec4f267bb6700499dbcad1221f4ec83
matthew <matthewenglish16@gmail.com>
2024-05-30 14:08:37 +0900

    completed cypher program and works as expected with simple checks, valgrind shows no mem leaks or error

--- day8_caesar.c
+++ day8_caesar.c
@@ -1,49 +1,133 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int check_error_arguements(int argc, char **arg);
-void help_message(void);
-void encrypt_message(char **n, char **arg);
+bool check_arguments(int argc, char **argv, char **flag, int *shift_amount, char **message);
+void free_memory(char *flag, char *message);
+void encrypt_message(int shift_amount, char *message);
+void decrypt_message(int shift_amount, char *message);
 
-int main(int argc, char**argv) {
+int main(int argc, char **argv) {
+	char *flag = NULL;
+	int shift_amount = 0;
+	char *message = NULL;
 
-	// TODO rework the program to parse each argument into its own variable during the arguments check at the start of the program
-	// dynamically allocate for strings as i cant be sure how long they will be from the start
-
-	if(check_error_arguements(argc, &argv[1])) {
+	if(!check_arguments(argc, argv, &flag, &shift_amount, &message)) {
+		// if errors in parsing args to variables return error and end program with 1;
+		free_memory(flag, message); // check to free any memory that may have been created in the process of parsing before fail
 		return 1;
 	}
-	if(strcmp(argv[1], "-h") == 0) {
-		help_message();
+	if(strcmp(flag, "-e") == 0) {
+		encrypt_message(shift_amount, message);
 	}
-	else if(strcmp(argv[1], "-e") == 0) {
-		encrypt_message(&argv[2], &argv[3]);
+	else if(strcmp(flag, "-d") == 0) {
+		decrypt_message(shift_amount, message);
 	}
-	else if(strcmp(argv[1], "-d") == 0) {
-		;
-	}
+
+	free_memory(flag, message);
 	return 0;
 }
 
-int check_error_arguements(int argc, char **arg) {
-	if(strcmp(*arg, "-h") == 0 && argc == 2) {
-		return 0;
+bool check_arguments(int argc, char **argv, char **flag, int *shift_amount, char **message) {
+	// check correct number of arguments & printing error
+	if(argc != 4) {
+		fprintf(stderr, "[ERROR]: Incorrect usage.\n[USAGE]: <program> <-e/-d> <shift_amount> <message>\n");
+		return false;
 	}
-	else if(argc < 4) {
-		fprintf(stderr, "ERROR: Insufficient arguements.\n");
-		printf("Usage: ./day8_caesar <-e/-d> <shift amount> <message>\n\t\"-h\" for help\n");
-		return 1;
+	for(int i = 1; i < argc; i++) {
+		switch(i) {
+		case 1:
+			// checking correct flag for encrypyt/dectrypt & printing error
+			if(strcmp(argv[1], "-e") != 0 && strcmp(argv[1], "-d") != 0) {
+				fprintf(stderr, "[ERROR]: Incorrect usage.\n[USAGE]: <program> <-e/-d> <shift_amount> <message>\n");
+				return false;
+			}
+			// allocate memory for flag variable to correct size & printing error if failed
+			*flag = malloc(strlen(argv[1]) + 1);
+			if(*flag == NULL) {
+				fprintf(stderr, "[ERROR]: Memory allocation fail.");
+				return false;
+			}
+			// copy from arg to variable
+			strcpy(*flag, argv[1]);
+			break;
+		case 2:
+			// converting to int and checking if natural number
+			*shift_amount = atoi(argv[2]);
+			if(*shift_amount < 1) {
+				fprintf(stderr, "[VALUE ERROR]: Please enter a value greater than 0.");
+				return false;
+			}
+			break;
+		case 3:
+			// allocate enough memory in variable to store message & print error if failed
+			*message = malloc(strlen(argv[3]) + 1);
+			if(*message == NULL) {
+				fprintf(stderr, "[ERROR]: Memory allocation fail.");
+				return false;
+			}
+			// copy from arg to variable
+			strcpy(*message, argv[3]);
+			break;
+		}
 	}
-	return 0;
+	return true;
 }
 
-void help_message(void) {
-	printf("[USAGE] ./day8_caesar <-e/-d> <shift amount> <message>\n");
-	printf("\t-e: encrypt\n\t-d: decrypt\n");
-	printf("\tshift amount: shift value by _ spaces\n");
-	printf("\tmessage: message to encrypt/decrypt\n");
+void free_memory(char *flag, char *message) {
+	// good practice to set pointers to NULL after freeing memory- stops them being used accidently 
+	if(flag != NULL) {
+		free(flag);
+		flag = NULL;
+	}
+	if(message != NULL) {
+		free(message);
+		message = NULL;
+	}
 }
 
-void encrypt_message(char **n, char **arg) {
-	;
+void encrypt_message(int shift_amount, char *message) {
+	/*
+		check if character is alpha, if not print as is
+		check if shifting character will put it out of bounds of A-Z/a-z and correctly wrapping the shift around by -26
+	*/
+	for(int i = 0; i < strlen(message); i++) {
+		if(isalpha(message[i])) {
+			if(isupper(message[i]) && message[i] + shift_amount > 'Z' 
+				|| islower(message[i]) && message[i] + shift_amount > 'z') {
+				printf("%c", (message[i] + shift_amount) - 26);
+			}
+			else {
+				printf("%c", message[i] + shift_amount);
+			}
+		}
+		else {
+			printf("%c", message[i]);
+		}	
+	}
+	printf("\n");
+}
+
+void decrypt_message(int shift_amount, char *message) {
+		/*
+		check if character is alpha, if not print as is
+		check if shifting character will put it out of bounds of A-Z/a-z and correctly wrapping the shift around by +26
+	*/
+	for(int i = 0; i < strlen(message); i++) {
+		if(isalpha(message[i])) {
+			if(isupper(message[i]) && message[i] - shift_amount < 'A' 
+				|| islower(message[i]) && message[i] - shift_amount < 'a') {
+				printf("%c", (message[i] - shift_amount) + 26);
+			}
+			else {
+				printf("%c", message[i] - shift_amount);
+			}
+		}
+		else {
+			printf("%c", message[i]);
+		}
+	}
+	printf("\n");
 }
