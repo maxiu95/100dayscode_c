74a252dd47ed064587555e40b15ba260c60911b0
Matthew English <matthewenglish16@gmail.com>
2024-06-01 12:53:45 +0000

    added functions and logic to check user input for name of new bidder

--- day9/day9_privateauction.c
+++ day9/day9_privateauction.c
@@ -4,41 +4,80 @@
 #include <string.h>
 
 
-#define NAME_LENGTH 35
+#define NAME_LENGTH 36
 #define MAX_BIDS 10
 
-struct Bid {
+struct Bidder {
 	char name[NAME_LENGTH];
 	double bid_amount;
 };
 
+void get_valid_name(struct Bidder *bidder_ptr); // acts as a verification loop until a correct value is passed by the user
+bool get_name_input(char *name); // directly takes input from user, returning true if meets correct conditions
+void get_valid_bid(struct Bidder *bidder_ptr);
+bool get_bid_input(double *amount);
+void get_valid_keep_bidding(void);
+
 int main(void) {
-	struct Bid bids[MAX_BIDS]; // store bid history
+	struct Bidder collected_bids[MAX_BIDS]; // store bid history
 	int num_bids = 0;
 	bool still_bidding = true;
-	char continue_bid[5] = "";
 	// collect bids clearing the screen after each one and storing as a struct in bids
 	// keep track of highest and lowest bids
 	// end of program print highest and lowest bidder as well as history of bids
 
-
 	while(still_bidding) {
-		if(num_bids == MAX_BIDS - 1) {
-			!still_bidding;
-		}
-		struct Bid new_bid;
-		printf("Welcome to the Secret Auction Program\n");
-		printf("What is your name? [Max 35 characters]: ");
-		scanf("%s", new_bid.name);
-		printf("What is your bid? : $");
-		scanf("%f", new_bid.bid_amount);
-		bids[num_bids] = new_bid;
+		struct Bidder new_bidder;
+		printf("Welcome to the 'Secret Auction'. Please place your bid.\n");
+		// collect user input and add to new_bidder struct
+		get_valid_name(&new_bidder);
+		get_valid_bid(&new_bidder);
+		// add to array
+		collected_bids[num_bids] = new_bidder;
 		num_bids++;
+		// break;
+		get_valid_keep_bidding();
+	}
+	for(int i = 0; i < num_bids; i++) {
+		printf("%s\n", collected_bids[i].name);
+	}
+}
 
-		printf("Any new bidders? (y/n): ");
-		scanf("%s", &continue_bid);
-		if(strcmp(continue_bid, "y")) {
-			still_bidding = false;
+void get_valid_name(struct Bidder *bidder_ptr) {
+	bool valid_input = false;
+	char name[NAME_LENGTH];
+	while(!valid_input) {
+		printf("Please enter your name (35 char MAX): ");
+		valid_input = get_name_input(name);
+		if(!valid_input) {
+			fprintf(stderr, "[INPUT ERROR]: Please input a valid name of maximum 35 characters.\n");
 		}
 	}
+	strncpy(bidder_ptr->name, name, NAME_LENGTH);
+}
+
+bool get_name_input(char *name) {
+	char input[100];
+	if(fgets(input, sizeof(input), stdin) == NULL) {
+		return false;
+	}
+	if(strlen(input) > NAME_LENGTH) {
+		return false;
+	}
+	// strcspn scans the string for newline char acting as an index as fgets also copies the newline from the enter key
+	input[strcspn(input, "\n")] = '\0';
+	strncpy(name, input, NAME_LENGTH);
+	return true;
+}
+
+void get_valid_bid(struct Bidder *bidder_ptr) {
+	;
+}
+
+bool get_bid_input(double *amount) {
+	;
+}
+
+void get_valid_keep_bidding(void) {
+	;
 }
